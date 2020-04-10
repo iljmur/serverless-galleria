0741333417c64ed2825db8ccc677e4b3bb4a38d2
iljmur <Iljmur4701>
2020-04-10 12:48:53 +0200

    rewrite uploader for serverless

---
+++ uploader/handler.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+
+const destBucket = process.env.DEST_BUCKET;
+
+exports.uploadAvatarImage = (event, context, lambdaCallback) => {
+  // Fail on mising data
+  if (!destBucket) {
+    context.fail('Error: Environment variable DEST_BUCKET missing');
+    return;
+  }
+
+  postImage(event, context, lambdaCallback);
+}
+
+function postImage(event, context, lambdaCallback) {
+  if (event.httpMethod === 'POST') {
+    let key = 'image.jpg'
+
+    // Get the body data
+    let body = Buffer.from(event.body, 'base64');
+
+    put(destBucket, key, body)
+      .then(() => {
+        const message = 'Saved ' + destBucket + ':' + key;
+        console.log(message);
+        done(200, JSON.stringify({ message: message }), 'application/json', lambdaCallback);
+      })
+      .catch((error) => {
+        console.error(error);
+        done(500, '{"message":"error saving"}', 'application/json', lambdaCallback);
+      });
+  }
+  else {
+    return done(400, '{"message":"Invalid HTTP Method "' + event.httpMethod + '}', 'application/json', lambdaCallback);
+  }
+}
+
+// We're done with this lambda, return to the client with given parameters
+function done(statusCode, body, contentType, lambdaCallback, isBase64Encoded = false) {
+  lambdaCallback(null, {
+    statusCode: statusCode,
+    isBase64Encoded: isBase64Encoded,
+    body: body,
+    headers: {
+      'Content-Type': contentType
+    }
+  });
+}
+
+// Create a promise to put the data in the s3 bucket
+function put(destBucket, destKey, data) {
+  return new Promise((resolve, reject) => {
+    s3.putObject({
+      Bucket: destBucket,
+      Key: destKey,
+      Body: data
+    }, (err, data) => {
+      if (err) {
+        console.error('Error putting object: ' + destBucket + ':' + destKey);
+        return reject(err);
+      }
+      else {
+        resolve(data);
+      }
+    });
+  });
+}
+
